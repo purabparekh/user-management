8623e53fde1688a0dda3a5a037dd6e06f9a2da2c
purabparekh <purabparekh@yahoo.com>
2019-04-19 19:38:33 +0530

    Updates

---
+++ precache-manifest.6fdb63fb544a814c10f292d00f3a7ae2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "996b449d104aef9193e0",
+    "url": "/user-management/static/css/main.5c1de10d.chunk.css"
+  },
+  {
+    "revision": "996b449d104aef9193e0",
+    "url": "/user-management/static/js/main.ec5ad411.chunk.js"
+  },
+  {
+    "revision": "d8cb7278ae3e524253ee",
+    "url": "/user-management/static/js/runtime~main.31a481db.js"
+  },
+  {
+    "revision": "d4f004e2c178b5862975",
+    "url": "/user-management/static/js/2.086abd68.chunk.js"
+  },
+  {
+    "revision": "d7b8c7b8587d89ecd12fb439b5caaa8f",
+    "url": "/user-management/index.html"
+  }
+];
